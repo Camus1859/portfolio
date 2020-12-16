fa9ca5e96b5f1069cd6fe34e95bd5049f6d24d2e
Anderson Laventure <Laventurepsu1@gmail.com>
2020-12-15 19:03:23 -0500

    updated mobile responsiveness

--- portfolio.js
+++ portfolio.js
@@ -5,6 +5,8 @@ const projectsContainer = document.querySelector('.projects-container')
 const bio = document.querySelector('.bio')
 const bio2 = document.querySelector('.bio2')
 const info = document.querySelectorAll('.info')
+const body2 = document.querySelector('.body')
+const html = document.querySelector('html')
 
 
 const displayMenuItems=(e)=>{
@@ -23,16 +25,22 @@ const displayMenuItems=(e)=>{
 const displayProjects=()=>{
   info.forEach(item=>(!item.classList.contains('projects-container') ?  item.classList.add('hidden') 
   : item.classList.remove('hidden')))
+
+
 } 
 
 const displayPrimaryInfo=()=>{
   info.forEach(item=>(!item.classList.contains('bio') ? item.classList.add('hidden') 
   : item.classList.remove('hidden')))
+
+
 }
 
 const displayAboutMe=()=>{
   info.forEach(item=>(!item.classList.contains('bio2') ? item.classList.add('hidden')
   : item.classList.remove('hidden')))
+
+
 }
   
 body.addEventListener('click', displayMenuItems)
